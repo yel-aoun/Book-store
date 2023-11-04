643399ff3ae7515df09c590b416ae21f153cd2d8
yel-aoun <youssef.elaouny1010@gmail.com>
2023-11-04 19:32:00 +0100

    compleet the project with functionalities of creat edit and delet book , working on adding a new format to show books

--- frontend/src/components/Spinner.jsx
+++ frontend/src/components/Spinner.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const Spinner = () => {
   return (
-    <div className=' animate-ping w-16 h-16 m-8 rounded-full bg-sky-600'></div>
-  )
-}
+    <div className='flex justify-center items-center h-screen'>
+      <div className='animate-ping w-16 h-16 rounded-full bg-green-500'></div>
+    </div>
+  );
+};
 
-export default Spinner
+export default Spinner;
--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -1,8 +1,77 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import BackButton from '../components/BackButton';
+import Spinner from '../components/Spinner';
+import axios from 'axios';
+import { useNavigate, useParams } from 'react-router-dom';
 
 const DeleteBook = () => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [publishYear, setPublishYear] = useState('');
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const {id} = useParams();
+  useEffect(()=>{
+    setLoading(true);
+    axios.get(`http://localhost:5555/books/${id}`)
+    .then((res)=>{
+      setTitle(res.data.title);
+      setAuthor(res.data.author);
+      setPublishYear(res.data.publishYear);
+      setLoading(false);
+    })
+    .catch((error)=>{
+      setLoading(false);
+      alert("an error happned, please check the console");
+      console.log(error);
+    });
+  }, [])
+  const Delete = () => {
+    setLoading(true);
+    axios.delete(`http://localhost:5555/books/${id}`)
+    .then(() => {
+      setLoading(false);
+      navigate('/');
+    })
+    .catch((error) =>  {
+      setLoading(false);
+      alert('An error happened, please check console');
+      console.log(error);
+    });
+  }
   return (
-    <div>DeleteBook</div>
+    <div className='p-4'>
+      <BackButton />
+      <h1 className='text-3xl my-4'> Delete Book</h1>
+      {loading ? <Spinner/> : (
+      <div className='flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto'>
+        <h3 className='flex justify-center items-center font-bold text-2xl'>Are you sure you want to delete this book</h3>
+        <div className='my-4'>
+          <label className='text-xl mr-4 text-gray-500'>Title</label>
+          <div
+            type='text'
+            className='border-2 border-gray-500 px-4 py-2 w-full'
+          >{title}</div>
+        </div>
+        <div className='my-4'>
+          <label className='text-xl mr-4 text-gray-500'>Author</label>
+          <div
+            type='text'
+            className='border-2 border-gray-500 px-4 py-2 w-full'
+          >{author}</div>
+        </div>
+        <div className='my-4'>
+          <label className='text-xl mr-4 text-gray-500'>PublishYear</label>
+          <div
+            type='text'
+            className='border-2 border-gray-500 px-4 py-2 w-full'
+          >{publishYear}</div>
+        </div>
+        <button onClick={Delete} className='bg-red-500 rounded-xl  hover:bg-sky-800 transition duration-2  m-8 p-2'> Delete</button>
+      </div>)}
+    </div>
   )
 }
+
 export default DeleteBook
+
